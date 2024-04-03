edfd41f71c2de2ee3128a1b569dee80a876ed494
Aayush Singh <97450185+aayushsingh7@users.noreply.github.com>
2024-04-03 22:22:02 +0530

    Improved UI

--- src/components/ProductBox.jsx
+++ src/components/ProductBox.jsx
@@ -4,6 +4,7 @@ import productData from "../json/searchResults.json";
 import { RiVerifiedBadgeFill } from "react-icons/ri";
 import { AppContext } from '../context/Context';
 import { Link } from 'react-router-dom';
+import { FaStar } from "react-icons/fa";
 import formatURL from '../utils/formatURL';
 
 const ProductBox = ({changePermit,min,product,productIndex}) => {
@@ -29,10 +30,10 @@ const ProductBox = ({changePermit,min,product,productIndex}) => {
           <Link to={formatURL(`/products/${product.type}/${product.name}`)} key={productIndex} className={styles.product_box_item}>
             <div className={styles.product_img}>
               <img src={product.main_image} alt={product.name} style={{objectFit:changePermit ? "cover" : "contain",padding:changePermit ? "0px": "10px"}}/>
-              {/* <p className={product.engraving ? styles.engrave : styles.engraven}> {product.engraving ? "✍️ Special Engraving Available" : ""}</p> */}
               <p className={styles.tag}>{product.tag}</p>
               <div className={styles.product_review}>
-                <p>⭐{product.reviewStars}</p>
+                <p style={{display:"flex",alignItems:"center"}} >
+                  <FaStar style={{marginRight:"2px",marginBottom:"3px",fontSize:"12px",color:"yellow"}}/>{product.reviewStars}</p>
                 <span>|</span>
                 <p style={{display:"flex",alignItems:"center"}}>{product.reviews} <RiVerifiedBadgeFill style={{fontSize:"13px",marginLeft:"2px",color:"#00da00"}}/></p>
               </div>
@@ -40,7 +41,7 @@ const ProductBox = ({changePermit,min,product,productIndex}) => {
             <div className={styles.product_box_content} style={{padding:changePermit ? "10px 10px 0px 10px" : "0px 10px"}}>
             
               <div className={styles.product_box_info}>
-                <h1>{product.name}</h1>
+                <h3>{product.name}</h3>
                 <div className={styles.product_price}>
                   <p className={styles.discount}>₹{(product.price).toLocaleString()}</p>
                   <p className={styles.discounto}>₹{(product.original_price).toLocaleString()}</p>
@@ -48,19 +49,22 @@ const ProductBox = ({changePermit,min,product,productIndex}) => {
                 </div>
               </div>
              
+            {
+              changePermit ? null : 
               <div className={styles.product_box_colors}
-               style={{height:changePermit ? "auto" : "50px",overflowY:"scroll"}}
-               >
-                <div className={styles.product_specialities}>
-                  {product.features &&  product.features.map((feature, index) => (
-                    <span key={index}>{feature}</span>
-                  ))}
-                </div>
+              style={{height:changePermit ? "auto" : "65px",overflowY:"scroll"}}
+              >
+               <div className={styles.product_specialities}>
+                 {product.features &&  product.features.map((feature, index) => (
+                   <span key={index}>{feature}</span>
+                 ))}
+               </div>
 
-              </div>
+             </div>
+            }
             </div>
-            <button onClick={()=> changePermit ? removeFromCart(product.id) : null } className={`${changePermit ? styles.remove_cart : styles.view_product} ${styles.btn_btn}`}>
-              {changePermit ? "Remove from Cart"  : "View Product"}</button>
+            <button  className={`${changePermit ? styles.remove_cart : styles.view_product} ${styles.btn_btn}`}>
+              {changePermit ? "Remove from Cart"  : "Add to Cart"}</button>
           </Link>
        
      
@@ -68,3 +72,4 @@ const ProductBox = ({changePermit,min,product,productIndex}) => {
 };
 
 export default ProductBox;
+// onClick={()=>  removeFromCart(product.id)}
